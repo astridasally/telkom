5be11e1e4b66d855783200db9aab8d9c6761cc95
Naurafatin Najihah Li Layyina <naurafatin24@gmail.com>
2025-07-17 14:06:06 +0700

    report part 1

--- resources/views/project_report.blade.php
+++ resources/views/project_report.blade.php
@@ -1,94 +1,107 @@
 <x-app-layout>
+<link rel="stylesheet" href="{{ asset('css/custom.css') }}">
 
     <h1>Mitra {{ Auth::user()->name }} </h1>
     <br>
 
 
-    <table>
-        <thead>
-            <tr>
-                <th rowspan="2">No</th>
-                @if(auth()->user()->role === 'mitra')
-                <th rowspan="2">Regional</th>
-                <th rowspan="2">Witel</th>
-                <th rowspan="2">Priority</th>
-                <th rowspan="2">Drop</th>
-                <th colspan="2" class="header-group">SURVEY</th>
-                <th colspan="2" class="header-group">DELIVERY</th>
-                <th colspan="2" class="header-group">INSTALASI</th>
-                <th colspan="2" class="header-group">INTEGRASI</th>
-                <th rowspan="2">Bukti Drop</th>
-                <th rowspan="2">Catuan ID</th>
-                <th rowspan="2">IHLD</th>
-                <th rowspan="2">Remark</th>
-                @endif
-                
-                @if(auth()->user()->role === 'vendor')
-                <th rowspan="2">Priority TA</th>
-                <th rowspan="2">Status OSP</th>
-                <th rowspan="2">Dependensi</th>
-                <th rowspan="2">Scenario Uplink</th>
-                <th rowspan="2">Status Uplink</th>
-                @endif
-                <th rowspan="2">Action</th>
-            </tr>
-            <tr>
-                <th>Plan</th>
-                <th>Realisasi</th>
-                <th>Plan</th>
-                <th>Realisasi</th>
-                <th>Plan</th>
-                <th>Realisasi</th>
-                <th>Plan</th>
-                <th>Realisasi</th>
-            </tr>
-        </thead>
-
-        <tbody>
-            @php $no = 1; @endphp
-            @foreach($projects as $project)
-            <tr>
-                <td scope="row">{{ $no++ }}</td>
-                @if(auth()->user()->role === 'vendor')
-                <td rowspan="2"><b> {{ $project->user->name }}</b></td>
-                @endif
-                <td scope="row">{{ $project->regional }}</td>
-                <td scope="row">{{ $project->witel }}</td>
-                <td scope="row">{{ $project->priority }}</td>
-                <td scope="row">{{ $project->drop_data }}</td>
-                <td scope="row">{{ $project->plan_survey }}</td>
-                <td scope="row">{{ $project->realisasi_survey }}</td>
-                <td scope="row">{{ $project->plan_delivery }}</td>
-                <td scope="row">{{ $project->realisasi_delivery }}</td>
-                <td scope="row">{{ $project->plan_instalasi }}</td>
-                <td scope="row">{{ $project->realisasi_instalasi }}</td>
-                <td scope="row">{{ $project->plan_integrasi }}</td>
-                <td scope="row">{{ $project->realisasi_integrasi }}</td>
-                <td scope="row">
-                    @if ($project->bukti_drop)
-                    <a href="{{ asset('storage/' . $project->bukti_drop) }}" target="_blank">Lihat File</a>
-                    @else
-                    Tidak Ada
-                    @endif
-                </td>
-                <td scope="row">{{ $project->catuan_id }}</td>
-                <td scope="row">{{ $project->ihld }}</td>
-                <td scope="row">{{ $project->remark }}</td>
-                @if(auth()->user()->role === 'vendor')
-                <td scope="row">{{ $project->priority_ta }}</td>
-                <td scope="row">{{ $project->status_osp }}</td>
-                <td scope="row">{{ $project->dependensi }}</td>
-                <td scope="row">{{ $project->scenario_uplink }}</td>
-                <td scope="row">{{ $project->status_uplink }}</td>
-                @endif
-                <td scope="row">
-                    <a href="{{ route('project_update_vendor', $project->id) }}">Edit</a>
-                </td>
-            </tr>
-            @endforeach
-        </tbody>
+    <table class="table-auto w-full border border-gray-400">
+    <thead class="border border-gray-400">
+    <tr>
+        <th rowspan="2">No</th>
+        <th rowspan="2">Regional</th>
+        <th rowspan="2">Witel</th>
+        <th rowspan="2">STO</th>
+        <th rowspan="2">Site</th>
 
 
+        @if(auth()->user()->role === 'mitra' || auth()->user()->role === 'admin')
+            <th rowspan="2">Priority</th>
+    
+            <th colspan="2" class="header-group">SURVEY</th>
+            <th colspan="2" class="header-group">DELIVERY</th>
+            <th colspan="2" class="header-group">INSTALASI</th>
+            <th colspan="2" class="header-group">INTEGRASI</th>
+            <th rowspan="2">Drop</th>
+            <th rowspan="2">Bukti Drop</th>
+        @endif
+
+        @if(auth()->user()->role === 'vendor' || auth()->user()->role === 'admin')
+            <th rowspan="2">Priority TA</th>
+            <th rowspan="2">Status OSP</th>
+            <th rowspan="2">Dependensi</th>
+            <th rowspan="2">Scenario Uplink</th>
+            <th rowspan="2">Status Uplink</th>
+        @endif
+
+        <th rowspan="2">Catuan ID</th>
+        <th rowspan="2">IHLD</th>
+        <th rowspan="2">Remark</th>
+        <th rowspan="2">Action</th>
+    </tr>
+
+    {{-- BARIS 2 - hanya ditampilkan kalau role MITRA/ADMIN --}}
+    @if(auth()->user()->role === 'mitra' || auth()->user()->role === 'admin')
+    <tr>
+        <th>Plan</th>
+        <th>Realisasi</th>
+        <th>Plan</th>
+        <th>Realisasi</th>
+        <th>Plan</th>
+        <th>Realisasi</th>
+        <th>Plan</th>
+        <th>Realisasi</th>
+    </tr>
+    @endif
+</thead>
+
+<tbody>
+    @php $no = 1; @endphp
+    @foreach($projects as $project)
+    <tr>
+        <td>{{ $no++ }}</td>
+         <td>{{ $project->regional }}</td>
+        <td>{{ $project->witel }}</td>
+        <td>{{ $project->sto }}</td>
+        <td>{{ $project->site }}</td>
+
+        @if(auth()->user()->role === 'mitra' || auth()->user()->role === 'admin')
+            <td>{{ $project->priority }}</td>
+            <td>{{ $project->survey_plan }}</td>
+            <td>{{ $project->survey_real }}</td>
+            <td>{{ $project->delivery_plan }}</td>
+            <td>{{ $project->delivery_real }}</td>
+            <td>{{ $project->instalasi_plan }}</td>
+            <td>{{ $project->instalasi_real }}</td>
+            <td>{{ $project->integrasi_plan }}</td>
+            <td>{{ $project->integrasi_real }}</td>
+            <td>{{ $project->drop }}</td>
+            <td>
+                @if($project->bukti_drop)
+                    <a href="{{ asset('storage/bukti_drop/' . $project->bukti_drop) }}" target="_blank">Lihat</a>
+                @else
+                    -
+                @endif
+            </td>
+        @endif
+
+        @if(auth()->user()->role === 'vendor' || auth()->user()->role === 'admin')
+            <td>{{ $project->priority_ta }}</td>
+            <td>{{ $project->status_osp }}</td>
+            <td>{{ $project->dependensi }}</td>
+            <td>{{ $project->scenario_uplink }}</td>
+            <td>{{ $project->status_uplink }}</td>
+        @endif
+
+        <td>{{ $project->catuan_id }}</td>
+        <td>{{ $project->ihld }}</td>
+        <td>{{ $project->remark }}</td>
+        <td><a href="{{ route('project_update', $project->id) }}">Edit</a></td>
+    </tr>
+    @endforeach
+</tbody>
+
+      
     </table>
 
 
