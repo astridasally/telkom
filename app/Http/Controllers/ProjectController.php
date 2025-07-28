b59554987850316bbc22a3a486c021ae45795c8d
Astrida Zahwa <astridazahwa@gmail.com>
2025-07-28 08:59:02 +0700

    edit report

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -142,7 +142,7 @@ class ProjectController extends Controller
 
         $query->orderBy('created_at', 'desc');
 
-        $projects = $query->paginate(10)->appends($request->query());
+        $projects = $query->paginate(18)->appends($request->query());
 
         // --- PASTIKAN BAGIAN INI ADA DAN BENAR ---
         // Untuk mengisi dropdown di view, kita perlu mendapatkan daftar unik Regional, Witel, STO
@@ -217,7 +217,7 @@ class ProjectController extends Controller
 
         // Ambil proyek dengan paginasi
         // Pagination limit diseragamkan menjadi 10 (seperti yang ada di logika pencarian)
-        $projects = $query->paginate(10)->appends($request->query()); // appends() untuk mempertahankan parameter search
+        $projects = $query->paginate(18)->appends($request->query()); // appends() untuk mempertahankan parameter search
 
         return view('project_report', compact('projects'));
     }
