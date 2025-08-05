94c451526692757e8e54c83421972e3f9b1059ff
Naurafatin Najihah Li Layyina <naurafatin24@gmail.com>
2025-08-05 17:28:07 +0700

    login aneh

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -39,7 +39,7 @@
                 </a>
             @endif
 
-            <x-primary-button class="ms-3">
+            <x-primary-button class="ms-3 bg-[#d46a28] hover:bg-[#bb551a] focus:ring-[#d46a28]">
                 {{ __('Log in') }}
             </x-primary-button>
 
